3de319fc93a4b7dd2e3e596bae606f0e04c396cb
Yağız Arslan <107375770+YagizWasTaken@users.noreply.github.com>
2023-04-09 15:29:46 +0300

    sensörler stabilize edildi

--- ultrasonic-sensors.ino
+++ ultrasonic-sensors.ino
@@ -1,24 +1,24 @@
-int sensor1_trig = 10;
-int sensor1_echo = 5;
-int sensor2_trig = 9;
-int sensor2_echo = 4;
-int sensor3_trig = 8;
-int sensor3_echo = 3;
-int sensor4_trig = 11;
-int sensor4_echo = 7;
-int sensor5_trig = 12;
-int sensor5_echo = 6;
+const int sensor1_trig = 2;
+const int sensor1_echo = 3;
+const int sensor2_trig = 4;
+const int sensor2_echo = 5;
+const int sensor3_trig = 7;
+const int sensor3_echo = 6;
+const int sensor4_trig = 9;
+const int sensor4_echo = 8;
+const int sensor5_trig = 10;
+const int sensor5_echo = 11;
 
 long sure1;
 long sure2;
 long sure3;
 long sure4;
 long sure5;
-long uzaklik1;
-long uzaklik2;
-long uzaklik3;
-long uzaklik4;
-long uzaklik5;
+int uzaklik1;
+int uzaklik2;
+int uzaklik3;
+int uzaklik4;
+int uzaklik5;
 
 void setup(){
   pinMode(sensor1_trig, OUTPUT);
@@ -33,6 +33,7 @@ void setup(){
   pinMode(sensor5_echo, INPUT);
   Serial.begin(9600);
 }
+
 void loop(){
   digitalWrite(sensor1_trig, LOW);
   digitalWrite(sensor2_trig, LOW);
@@ -40,46 +41,54 @@ void loop(){
   digitalWrite(sensor4_trig, LOW);
   digitalWrite(sensor5_trig, LOW);
   delayMicroseconds(5);
-  digitalWrite(sensor1_echo, HIGH);
-  digitalWrite(sensor2_echo, HIGH);
-  digitalWrite(sensor3_echo, HIGH);
-  digitalWrite(sensor4_echo, HIGH);
-  digitalWrite(sensor5_echo, HIGH);
+  digitalWrite(sensor1_trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(sensor1_trig, LOW);
-  digitalWrite(sensor2_trig, LOW);
-  digitalWrite(sensor3_trig, LOW);
-  digitalWrite(sensor4_trig, LOW);
-  digitalWrite(sensor5_trig, LOW);
   sure1 = pulseIn(sensor1_echo, HIGH);
+  uzaklik1 = sure1 * 0.034 / 2;
+
+  digitalWrite(sensor2_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensor2_trig, LOW);
   sure2 = pulseIn(sensor2_echo, HIGH);
+  uzaklik2 = sure2 * 0.034 / 2;
+
+  digitalWrite(sensor3_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensor3_trig, LOW);
   sure3 = pulseIn(sensor3_echo, HIGH);
+  uzaklik3 = sure3 * 0.034 / 2;
+
+  digitalWrite(sensor4_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensor4_trig, LOW);
   sure4 = pulseIn(sensor4_echo, HIGH);
+  uzaklik4 = sure4 * 0.034 / 2;
+
+  digitalWrite(sensor5_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensor5_trig, LOW);
   sure5 = pulseIn(sensor5_echo, HIGH);
-  uzaklik1= sure1 /29.1/2;
-  uzaklik2= sure2 /29.1/2;
-  uzaklik3= sure3 /29.1/2;
-  uzaklik4= sure4 /29.1/2;
-  uzaklik5= sure5 /29.1/2;
-  
-  if(uzaklik1 > 200)
-    Serial.print("uzaklik1 ");
-    Serial.print(uzaklik1);
-  Serial.print("/");
-  if(uzaklik2 > 200) 
-    Serial.print("uzaklik2 ");
-    Serial.print(uzaklik2);
-  Serial.print("/");
-  if(uzaklik3 > 200)
-    Serial.print("uzaklik3 ");
-    Serial.print(uzaklik3);
-  Serial.print("/");
-  if(uzaklik4 > 200)
-    Serial.print("uzaklik4 ");
-    Serial.print(uzaklik4);
-  Serial.print("/");
-  if(uzaklik5 > 200)
-    Serial.print("uzaklik5 ");
-    Serial.println(uzaklik5);
-  delay(100);
+  uzaklik5 = sure5 / 29.1 / 2;
+
+  Serial.print("Uzaklik1: ");
+  Serial.print(uzaklik1);
+  Serial.print(" cm, ");
+
+  Serial.print("Uzaklik2: ");
+  Serial.print(uzaklik2);
+  Serial.print(" cm, ");
+
+  Serial.print("Uzaklik3: ");
+  Serial.print(uzaklik3);
+  Serial.print(" cm, ");
+
+  Serial.print("Uzaklik4: ");
+  Serial.print(uzaklik4);
+  Serial.print(" cm, ");
+
+
+  Serial.print("Uzaklik5: ");
+  Serial.print(uzaklik5);
+  Serial.println(" cm");
 }
